29afaf26fd68903b07110be3907ec813db92ce28
Edson Ray San Juan <qeresanjuan@tip.edu.ph>
2026-04-19 05:16:20 +0800

    config

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,6 +1,6 @@
 const API_BASE_URL =
   process.env.NODE_ENV === "development"
     ? "http://127.0.0.1:5000/api"
-    : "https://softdes2-backend.onrender.com/api";
+    : "https://softdes-finalproj.onrender.com/api";
 
 export default API_BASE_URL;
